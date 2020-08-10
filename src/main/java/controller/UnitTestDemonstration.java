9f87d5f8c7ed0ac5e23bfe278f1c7d114da02e7f
William Duong <w1702@Williams-MacBook-Pro.local>
2020-08-10 16:03:48 +1000

    Added mongodb and unit test libraries. Added skeleton models

---
+++ src/main/java/controller/UnitTestDemonstration.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class UnitTestDemonstration {
+    private int x;
+
+    public UnitTestDemonstration(int x){
+        this.x = x;
+    }
+
+    public int getX(){
+        return x;
+    }
+}
