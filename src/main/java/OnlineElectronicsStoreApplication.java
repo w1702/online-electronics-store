6cac8821faa74d5231a3945c5196396b73f3fc69
William Duong <w1702@Williams-MacBook-Pro.local>
2020-08-11 12:06:10 +1000

    added javafx library

---
+++ src/main/java/OnlineElectronicsStoreApplication.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OnlineElectronicsStoreApplication extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+    }
+}
