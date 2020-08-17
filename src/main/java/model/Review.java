5b244520ed26a05dfd58c489cfd500d0d398e931
William Duong <w1702@Williams-MacBook-Pro.local>
2020-08-17 21:34:28 +1000

    in progress changes

--- src/main/java/model/Review.java
+++ src/main/java/model/Review.java
@@ -1,4 +1,30 @@
 package model;
 
+/**
+ * This class represents a review given to an Item
+ */
 public class Review {
+    private String id;
+    private String date;
+    private String comment;
+    private User user;
+
+    public Review(){
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
---
+++ src/test/java/db/DatabaseClientIntegrationTest.java
@@ -0,0 +1,19 @@
+package db;
+
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseClientIntegrationTest {
+    @Test
+    public void givenDBCredentials_whenConnectionEstablished_thenVerifyDocumentsAreRetrived(){
+        DatabaseClient databaseClient = new DatabaseClient();
+        Document appData = databaseClient.getAppData();
+
+
+
+    }
+}
