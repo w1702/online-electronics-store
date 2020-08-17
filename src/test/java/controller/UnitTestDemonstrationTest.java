d128711cac2117e50fb51438e08ee6feab3c115b
William Duong <w1702@Williams-MacBook-Pro.local>
2020-08-17 19:58:16 +1000

    changed sample unit test

--- src/test/java/controller/UnitTestDemonstrationTest.java
+++ src/test/java/controller/UnitTestDemonstrationTest.java
@@ -6,9 +6,15 @@ import org.junit.jupiter.api.Test;
 class UnitTestDemonstrationTest {
     @Test
     public void whenCreatingClassWithX_thenReturnXInGetter(){
+
+        // 1 arrange - setup data or input to your function
+        int expected = 1;
+        // 2 act - calling your function
         // Create a new instance of the class
-        UnitTestDemonstration unitTestDemonstration = new UnitTestDemonstration(1);
+        UnitTestDemonstration unitTestDemonstration = new UnitTestDemonstration(expected);
+        int result = unitTestDemonstration.getX();
+        // assert - checking the output of your function
         // Check if expected value equals function return value
-        Assertions.assertEquals(1, unitTestDemonstration.getX());
+        Assertions.assertEquals(1, result);
     }
 }
