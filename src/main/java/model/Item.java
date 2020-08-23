e2b886e2871f5ca29574f431f029d1d39b806cc0
Chak Wing Lam <chakwing3@yahoo.com.hk>
2020-08-23 15:54:48 +1000

    Midify item model

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -3,5 +3,35 @@ package model;
 import java.util.List;
 
 public class Item {
-    //private List<Review> reviews;
+    private List<Review> reviews;
+    private String id, name, image, description;
+    private Double cost;
+
+    public Item(){
+
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public Double getCost(){
+        return cost;
+    }
+
+    public List<Review> getReviews(){
+        return reviews;
+    }
 }
