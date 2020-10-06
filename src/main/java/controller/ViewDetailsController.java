3b03690c680ba85a6843b70f64f6201893884d29
William Duong <w1702@williams-macbook-pro.local>
2020-10-06 17:24:28 +1100

    added quantity in cart information

--- src/main/java/controller/ViewDetailsController.java
+++ src/main/java/controller/ViewDetailsController.java
@@ -7,6 +7,7 @@ import javafx.scene.text.Text;
 import model.Item;
 import model.OnlineElectronicsStore;
 import utils.MVCController;
+import javafx.scene.control.TextField;
 
 
 public class ViewDetailsController extends MVCController<OnlineElectronicsStore> {
@@ -30,15 +31,23 @@ public class ViewDetailsController extends MVCController<OnlineElectronicsStore>
     private Text addToCartStatusText;
 
     @FXML
-    private javafx.scene.control.TextField reviewCommentTextField;
+    private TextField reviewCommentTextField;
 
-    @FXML private void initialize(){ ;
+    @FXML
+    private Text addReviewCommentStatusText;
+
+    @FXML
+    private Text quantityInCartText;
+
+
+    @FXML private void initialize(){
         Item item = getCurrentItem();
         itemImageView.setImage(item.getConvertBase64toImage());
         idText.setText("ID: " + item.getId());
         nameText.setText("name: " + item.getName());
         costText.setText("cost: " + item.getCost());
         descriptionText.setText("description: " + item.getDescription());
+        setQuantityInCartText();
     }
     
     @FXML
@@ -48,17 +57,32 @@ public class ViewDetailsController extends MVCController<OnlineElectronicsStore>
                 .getShoppingCart()
                 .addItem(getCurrentItem().getId(), 1);
         addToCartStatusText.setText("Added item to cart");
+        setQuantityInCartText();
     }
 
     @FXML
     public void handleAddReview(){
-        getModel()
-                .getItemById(getModel().getCurrentlySelectedItem())
-                .addReview(getModel().getLoggedInUser().getId(), reviewCommentTextField.getText());
-        DatabaseWriteClient.writeReviewsToDB();
+        if(reviewCommentTextField.getText().isEmpty()){
+            addReviewCommentStatusText.setText("Please enter a review comment");
+        }
+        else{
+            addReviewCommentStatusText.setText("Review comment added");
+            getModel()
+                    .getItemById(getModel().getCurrentlySelectedItem())
+                    .addReview(getModel().getLoggedInUser().getId(), reviewCommentTextField.getText());
+            DatabaseWriteClient.writeReviewsToDB();
+        }
     }
 
     public Item getCurrentItem(){
         return getModel().getItemById(getModel().getCurrentlySelectedItem());
+    }
+
+    private void setQuantityInCartText() {
+        Integer quantity = getModel().getLoggedInUser().getShoppingCart().getItemQuantity().get(getCurrentItem().getId());
+        if(quantity == null){
+            quantity = 0;
+        }
+        quantityInCartText.setText("quantity in shopping cart: " + quantity);
     }
 }
